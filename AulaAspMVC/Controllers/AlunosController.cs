7f51c537c46ae471e424128a8e684ff8e424eca8
CASSIO RODRIGO SOARES DO PRADO <cassio.rsprado@senacsp.edu.br>
2025-01-31 16:29:04 -0300

    Funcionalidade Alunos

---
+++ AulaAspMVC/Controllers/AlunosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AulaAspMVC.Controllers
+{
+    public class AlunosController : Controller
+    {
+        public IActionResult Cassio()
+        {
+            return View();
+        }
+    }
+}
